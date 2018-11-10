a7458bf0c5bf7c445224101a5f764e5b96496ba8
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-10 11:38:02 +0100

    added all tests and got green, added some styling

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,30 +1,30 @@
-function FizzBuzz()  {
-    this.check = (number) => {
-        return number
-    }
-}
+// function FizzBuzz()  {
+//     this.check = (number) => {
+//         return number
+//     }
+// }
 
-function FizzBuzz() {
-    this.check = (number) => {
-        if (number % 3 === 0) {
-            return 'Fizz';
-        } else {
-            return number
-        }
-    }
-}
+// function FizzBuzz() {
+//     this.check = (number) => {
+//         if (number % 3 === 0) {
+//             return 'Fizz';
+//         } else {
+//             return number
+//         }
+//     }
+// }
 
-function FizzBuzz() {
-    this.check = (number) => {
-        if (number % 5 === 0) {
-            return 'Buzz';
-        } else if (number % 3 === 0) {
-            return 'Fizz';
-        } else {
-            return number
-        }
-    }
-}
+// function FizzBuzz() {
+//     this.check = (number) => {
+//         if (number % 5 === 0) {
+//             return 'Buzz';
+//         } else if (number % 3 === 0) {
+//             return 'Fizz';
+//         } else {
+//             return number
+//         }
+//     }
+// }
 
 function FizzBuzz() {
     this.check = (number) => {
